3e18aee444a7cbb1c3b77d01da0677a7b57135f3
AgungPriyadi <AgungPriyadi@NB-GDA-55026>
2026-06-03 10:57:57 +0700

    update listeners

--- Test Listeners/AutoScreenshotListener.groovy
+++ Test Listeners/AutoScreenshotListener.groovy
@@ -43,7 +43,7 @@ class AutoScreenshotListener {
 
 	/**
 	 * Mendapatkan timestamp saat ini dalam format yyyyMMdd_HHmmss
-	 * @return String timestamp
+	 * @return String timestam
 	 */
 	String getTimestamp() {
 		return new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())
